524b8d84bb7a4621c36f0eaf377c668528ad89ef
Akram-s <akram.sa18@gmail.com>
2019-08-30 11:49:01 +0530

    change of classname

--- src/test/java/com/StudentControllerTest.java
+++ src/test/java/com/StudentControllerTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class HelloControllerTest {
+public class StudentControllerTest {
 
     @Autowired
     private MockMvc mvc;
